fe4b74ca91852329ab2284329979619c647db802
Zakhar Voloshchuk (3elDU) <3elduu@gmail.com>
2023-03-05 12:28:09 +0200

    Added more options and functionality

--- main.py
+++ main.py
@@ -10,18 +10,27 @@ tokens_consumed: int = 0
 def main(args):
   global tokens_consumed
 
-  message_history = [
-    {"role": "user", "content": "Convert natural language into commands in terminal\nPrefix all runnable commands by '!' sybmol, and print them on separate line\nBe short and concise"},
-    # {"role": "user", "content": "Convert natural language into terminal commands, and describe their output."},
-    {"role": "user", "content": "What system am I running on?"},
-    {"role": "assistant", "content": "This command will print the kernel you're running:\n!uname -a"},
-    {"role": "system", "content": "Linux ebk1040g3 6.2.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Sun, 26 Feb 2023 03:39:23 +0000 x86_64 GNU/Linux"},
-    {"role": "assistant", "content": "Your computer is running Arch GNU/Linux 6.2.1"},
-    # {"role": "user", "content": args.prompt},
-  ]
+  message_history = []
+
+  if not args.empty:
+    message_history = [
+      {"role": "user", "content": "Convert natural language into commands in terminal\nPrefix all runnable commands by '!' sybmol, and print them on separate line\nBe short and concise"},
+      # {"role": "user", "content": "Convert natural language into terminal commands, and describe their output."},
+      {"role": "user", "content": "What system am I running on?"},
+      {"role": "assistant", "content": "This command will print the kernel you're running:\n!uname -a"},
+      {"role": "system", "content": "Linux ebk1040g3 6.2.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Sun, 26 Feb 2023 03:39:23 +0000 x86_64 GNU/Linux"},
+      {"role": "assistant", "content": "Your computer is running Arch GNU/Linux 6.2.1"},
+      # {"role": "user", "content": args.prompt},
+    ]
 
   while True:
-    prompt = input("prompt: ")
+    if args.prompt:
+      prompt = args.prompt
+      # clear the prompt for next iterations
+      args.prompt = None
+    else:
+      prompt = input("Prompt: ")
+    
     message_history.append({"role": "user", "content": prompt})
 
     # 1st stage ( AI parsing natural language, converting it into shell command )
@@ -42,11 +51,13 @@ def main(args):
       command = line[1:]
       commands_to_run.append(command)
 
-    print(commands_to_run)
+    for i, command in enumerate(commands_to_run):
+      print(f"{i}. {command}")
 
-    # for i, command in enumerate(commands_to_run):
-    #   print(f"{i}; {command}")
-    answer = input("Is it ok to run those commands? y/n/s: ")
+    if args.run:
+      answer = 'y'
+    else:
+      answer = input("Is it ok to run those commands? y/n/s: ")
 
     if answer.lower() == 's':
       continue
@@ -57,11 +68,11 @@ def main(args):
 
     for command in commands_to_run:
       # 2nd stage ( running the command )
-      output = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.decode()
-      # print(output)
+      result = subprocess.run(command, shell=True, stdout=subprocess.PIPE)
+      output = result.stdout.decode()
       message_history.append({
         "role": "system",
-        "content": output
+        "content": output + f"\nExit code: {result.returncode}"
       })
       print(output)
 
@@ -75,10 +86,11 @@ def main(args):
 
     print(completion.choices[0].message.content)
 
-  # for choice in completion.choices:
-  #   print(choice.message.content, '\n\n')
+    if args.oneshot:
+      break
 
-  # print("tokens consumed:", tokens_consumed)
+  if args.verbose:
+    print("tokens consumed:", tokens_consumed)
 
 if __name__ == '__main__':
   openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -86,10 +98,14 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('-t', '--temperature', help="How random the answer should be. Values from 0 to 2", type=float, default=0)
   parser.add_argument('-m', '--model', help="Model to use", default="gpt-3.5-turbo")
-  # parser.add_argument('prompt')
+  parser.add_argument('-1', '--oneshot', help="Run non-interactively", action='store_true')
+  parser.add_argument('-p', '--prompt', help="The prompt to use")
+  parser.add_argument('-y', '--run', help="(DANGEROUS) Run the command immediately", action='store_true')
+  parser.add_argument('-0', '--empty', help="Start with empty history", action='store_true')
+  parser.add_argument('-v', '--verbose', help="Be more verbose", action='store_true')
 
   args = parser.parse_args()
   try:
     main(args)
   except KeyboardInterrupt:
-    print("\n", tokens_consumed)
+    pass
